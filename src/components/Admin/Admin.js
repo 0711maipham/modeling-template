9a05632d602fe55b23bcd484c7189aa911955217
Tonette Pham <0711maipham@gmail.com>
2021-01-26 01:12:34 -0800

    added fields and pages

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Row, Col, Card } from "react-bootstrap"
+import { Container, Row, Col, Card } from "react-bootstrap"
 import Upload from './Upload'
 import EditProfile from './EditProfile'
 
@@ -7,36 +7,38 @@ function Admin(props) {
     const { profile } = props;
 
     return (
-        <Row>
-            <Col sm={{ span: 10, offset: 1 }}>
-            <Card>
-                <Card.Header>Edit Profile</Card.Header>
-                <Card.Body>
-                    <EditProfile
-                        profile={profile}
-                    />
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Header>Edit Portfolio</Card.Header>
-                <Card.Body>
-                    <Upload
-                        profile={profile}
-                        destination={"gallery"}
-                    />
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Header>Edit Digitals</Card.Header>
-                <Card.Body>
-                    <Upload
-                        profile={profile}
-                        destination={"digitals"}
-                    />
-                </Card.Body>
-            </Card>
-            </Col>
-        </Row>
+        <Container>
+            <Row>
+                <Col sm="12">
+                    <Card>
+                        <Card.Header>Edit Profile</Card.Header>
+                        <Card.Body>
+                            <EditProfile
+                                profile={profile}
+                            />
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Header>Edit Portfolio</Card.Header>
+                        <Card.Body>
+                            <Upload
+                                profile={profile}
+                                destination={"gallery"}
+                            />
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Header>Edit Digitals</Card.Header>
+                        <Card.Body>
+                            <Upload
+                                profile={profile}
+                                destination={"digitals"}
+                            />
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     )
 };
 
